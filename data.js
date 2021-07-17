ff3f82d041a8235cab9f6596a59ad3947398919f
Haris Dolovac <dharis90@gmail.com>
2021-07-17 10:17:31 +0200

    initial commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    category: "CATEGORY",
+    title: "RAcalte 1 ",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, ex? Autem ipsa fugiat, voluptatibus amet hic labore animi vel voluptates.",
+    button: "Learn more ->",
+  },
+  {
+    category: "CATEGORY",
+    title: "RAcalte 2 ",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, ex? Autem ipsa fugiat, voluptatibus amet hic labore animi vel voluptates.",
+    button: "Learn more ->",
+  },
+  {
+    category: "CATEGORY",
+    title: "RAcalte 3 ",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, ex? Autem ipsa fugiat, voluptatibus amet hic labore animi vel voluptates.",
+    button: "Learn more ->",
+  },
+];
+
+export default data;
